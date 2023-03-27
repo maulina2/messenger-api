963fee94d11fc767447af06223493e30b1c184c5
ZubanovaAlina <alinazubanova5@gmail.com>
2023-03-27 18:02:37 +0700

    Добавила новый метод для преобразования дто в сущность в UserMapper

--- src/main/java/ru/tsu/hits/messengerapi/mapper/UserMapper.java
+++ src/main/java/ru/tsu/hits/messengerapi/mapper/UserMapper.java
@@ -2,8 +2,12 @@ package ru.tsu.hits.messengerapi.mapper;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import ru.tsu.hits.messengerapi.dto.SignUpDto;
 import ru.tsu.hits.messengerapi.dto.UserDto;
 import ru.tsu.hits.messengerapi.entity.UserEntity;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -19,5 +23,23 @@ public class UserMapper {
                 user.getPatronymic(),
                 user.getSex()
         );
+    }
+
+    public UserEntity SignUpDtoToUser(SignUpDto signUpDto) {
+        return UserEntity
+                .builder()
+                .login(signUpDto.getLogin())
+                .sex(signUpDto.getSex())
+                .name(signUpDto.getName())
+                .surname(signUpDto.getSurname())
+                .birthDate(signUpDto.getBirthDate())
+                .patronymic(signUpDto.getPatronymic())
+                .creationDate(convertToDateViaSqlDate(LocalDate.now()))
+                .password(signUpDto.getPassword())
+                .build();
+    }
+
+    private Date convertToDateViaSqlDate(LocalDate dateToConvert) {
+        return java.sql.Date.valueOf(dateToConvert);
     }
 }
